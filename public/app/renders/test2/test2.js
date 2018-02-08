47f3845b5ff4dff210f8810176c1b475bcff3eed
Alberto Jesús Rodríguez Pulido <albrodpul@gmail.com>
2018-02-08 17:01:40 +0100

    URL fix in tests mvcs

--- public/app/renders/test2/test2.js
+++ public/app/renders/test2/test2.js
@@ -4,7 +4,7 @@ angular
       .module("renderApp")
       .controller("test2", function ($scope, $http) {
             console.log("Test2 Controller Initialized");
-            $http.get("http://localhost:8800/app/renders/test2/test2.json")
+            $http.get("http://ui-renders.herokuapp.com/app/renders/test2/test2.json")
                   .then(function (response) {
                         $scope.model = response.data.data[0];
                   });
