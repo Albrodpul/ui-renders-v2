4dde6751cc1e2b09e4c53bc1a0d25c9a6a88a3c1
Albrodpul <albrodpul@gmail.com>
2018-05-04 23:17:25 +0200

    Renderizer ui

--- public/renderizer-ui/app/app.module.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-angular.module('renderApp', [
-    'ui.router',
-    'ui.materialize'
-    ]);
--- public/renderizer-ui/app/states/home/home.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-angular
-      .module("renderApp")
-      .controller("HomeController", function ($scope, $http, $state) {
-                  console.log("Renderizer Ui Home Controller initialized");
-
-      });
